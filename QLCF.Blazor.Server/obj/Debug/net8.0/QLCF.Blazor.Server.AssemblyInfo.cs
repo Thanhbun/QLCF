f712a4160d68dcf2c1faf836885d14c1e3e7c833
Toàn Nhật <btoan9994@gmail.com>
2024-07-06 02:03:30 +0700

    'toanTask'

--- QLCF.Blazor.Server/obj/Debug/net8.0/QLCF.Blazor.Server.AssemblyInfo.cs
+++ QLCF.Blazor.Server/obj/Debug/net8.0/QLCF.Blazor.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLCF.Blazor.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13a760f1815650b346fd05ab32a20e47d2d3e5f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6338d0b365ef6d667994af49c99e26063ca21d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLCF.Blazor.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLCF.Blazor.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.*")]
--- QLCF.Module/BusinessObjects/databaseCode/TrangthaiTT.cs
+++ QLCF.Module/BusinessObjects/databaseCode/TrangthaiTT.cs
@@ -7,7 +7,7 @@ using System.ComponentModel;
 using System.Reflection;
 namespace QLCF.Module.BusinessObjects.database
 {
-    // Task Toàn
+    // Task NhatToan
     public partial class TrangthaiTT
     {
         public TrangthaiTT(Session session) : base(session) { }
